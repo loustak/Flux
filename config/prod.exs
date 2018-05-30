24517999ca6af3a0fc4086c4fdfcc84f2675252e
Loustak <sardois.lucas@gmail.com>
2018-05-30 22:57:17 +0200

    chore(CORS): Changed allowed origins

--- config/prod.exs
+++ config/prod.exs
@@ -16,7 +16,7 @@ config :flux, Flux.Endpoint,
   url: [scheme: "https", host: "https://flux-back.herokuapp.com/", port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE"),
-  check_origin: [] # my front end origins
+  check_origin: [System.get_env("ALLOWED_ORIGIN")] # my front end origins
 
 # Guardian configuration
 config :guardian, Guardian,
