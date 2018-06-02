f6d36a2c23c7ec5d0c9bc4c1eaa4fbf99a1560c0
Loustak <sardois.lucas@gmail.com>
2018-06-02 09:55:16 +0200

    feat(communities): Trigger to add the user who create a community inside the community.
    Also create a new default discussion inside the community.

--- priv/repo/migrations/20180601231523_create_triggers.exs
+++ priv/repo/migrations/20180601231523_create_triggers.exs
@@ -3,23 +3,36 @@ defmodule Flux.Repo.Migrations.Triggers do
 
   def change do
 
+    # Trigger 1: Create a default community for every new users
     execute "CREATE OR REPLACE FUNCTION create_default_community() RETURNS trigger AS
     $$
     DECLARE
       save_community_id bigint;
     BEGIN
       INSERT INTO communities(user_id, name, inserted_at, updated_at) values(NEW.id, NEW.username, now(), now()) RETURNING id into save_community_id;
-      INSERT INTO discussions(community_id, name, inserted_at, updated_at) values(save_community_id, 'Default', now(), now());
-      INSERT INTO user_communities(user_id, community_id, inserted_at, updated_at) values(NEW.id, save_community_id, now(), now());
       RETURN NEW;
     END;
     $$ LANGUAGE plpgsql;
     "
 
     execute "DROP TRIGGER IF EXISTS create_default_community on users;"
-
     execute "CREATE TRIGGER create_default_community AFTER INSERT ON users
     FOR EACH ROW
     EXECUTE PROCEDURE create_default_community();"
+
+    # Trigger 2: The user who create a community join it and create a default discussion
+    execute "CREATE OR REPLACE FUNCTION join_created_community() RETURNS trigger AS
+    $$
+    BEGIN
+      INSERT INTO discussions(community_id, name, inserted_at, updated_at) values(NEW.id, 'Main', now(), now());
+      INSERT INTO user_communities(user_id, community_id, inserted_at, updated_at) values(NEW.user_id, NEW.id, now(), now());
+      RETURN NEW;
+    END;
+    $$ LANGUAGE plpgsql;"
+
+    execute "DROP TRIGGER IF EXISTS trigger_join_created_community on communities;"
+    execute "CREATE TRIGGER trigger_join_created_community AFTER INSERT ON communities
+    FOR EACH ROW
+    EXECUTE PROCEDURE join_created_community();"
   end
 end
