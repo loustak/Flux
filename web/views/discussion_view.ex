534f825e845e1e0bffd5217c7b26f27cf6bb3e28
Loustak <sardois.lucas@gmail.com>
2018-06-02 14:28:51 +0200

    fix(communities): No more community_id when sending the discussion

--- web/views/discussion_view.ex
+++ web/views/discussion_view.ex
@@ -4,7 +4,6 @@ defmodule Flux.DiscussionView do
   def render("read.json", %{discussion: discussion}) do
     %{
       id: discussion.id,
-      community_id: discussion.community_id,
       name: discussion.name
     }
   end
